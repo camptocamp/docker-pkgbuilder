b819c9bafe80ee6c7faf3eb2177315b0af073516
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2016-06-22 16:46:28 +0200

    awscli: build package for wheezy + version upgrade

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ for i in Docker*; do
   image_name="$2-$3-$4"
   echo $image_name
   docker build -f $i -t $image_name .
-  container_id=$(docker run --detach $image_name)
-  mkdir -p build/$img_name
-  docker cp $container_id:/root/build build/$image_name
-  sleep 1
+  container_id=$(docker run -dit $image_name bash)
+  echo $container_id
+  mkdir -p $image_name
+  docker cp $container_id:/root/build ./$image_name
+  docker stop $container_id
   docker rm $container_id
   docker rmi $image_name
 done
